a6fcdf78548a74d71f8ca10a6d42bfd45a11aaef
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-02-03 14:30:39 -0800

    Insertion Sort

---
+++ Insertion Sort/main.cpp
@@ -0,0 +1,27 @@
+#include <array>
+#include <iostream>
+#include <cmath>
+using namespace std;
+void insertionSort(int arr[],int size){
+    for(int i=1;i<size;i++){
+        int prev=arr[i];
+        int k=i-1;
+        while(arr[k]>prev&&k>=0){
+            arr[k+1]=arr[k];
+            k--;
+        }
+        arr[k+1]=prev;
+    }
+    for(int i=0;i<size;i++){
+        cout << arr[i] << endl;
+    }
+}
+
+int main(){
+    int param[1000]={};
+    for(int i=0;i<1000;i++){
+        param[i]=rand()%1000;
+    }
+    insertionSort(param,sizeof(param)/sizeof(param[0]));
+    return 0;
+}
